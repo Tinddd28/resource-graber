66636fca475f6e37a93a8a4b3892f3753075428c
Tinddd28 <tind.nik.28@gmail.com>
2025-05-16 03:03:55 +0300

    upd: check efficiency of the network agent layer

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log/slog"
+	"resource-graber/internal/agents/network"
+	"resource-graber/internal/config"
+	service "resource-graber/internal/services"
+)
+
+func main() {
+	config := config.NewConfig()
+	// #TODO: write own logger
+	logger := slog.Logger{}
+
+	network := network.NewNetwork(&logger)
+	service := service.NewService(config, network, nil, nil, nil, &logger)
+	service.Run()
+
+}
