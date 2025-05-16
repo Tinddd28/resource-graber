fdb3307d70ce7614d628327eb874a9ab8d9c46db
Tinddd28 <tind.nik.28@gmail.com>
2025-05-16 03:01:33 +0300

    add: file for interaction with network agent

---
+++ internal/services/network.go
@@ -0,0 +1,17 @@
+package service
+
+import "resource-graber/internal/models"
+
+func (s *Service) ProcessNetwork() {
+	for {
+		_, err := s.GetNetwork()
+		if err != nil {
+			s.logger.Error("error getting network", "error", err)
+			continue
+		}
+	}
+}
+
+func (s *Service) GetNetwork() (models.Network, error) {
+	return models.Network{}, nil
+}
